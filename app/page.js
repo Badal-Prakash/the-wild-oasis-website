a4559b7cb685e47c435435e027c3bf47e51e1508
badal <badal@badals-MacBook-Air.local>
2024-10-12 19:33:22 +0530

    Navigation added

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 import Link from "next/link";
+import Navigation from "./component/navigation";
 
 export default function Home() {
   return (
     <div>
       <h1>The Wild Oasis welcome to paradise</h1>
-      <Link href="/cabins">Explore Luxury Cabins</Link>
     </div>
   );
 }
